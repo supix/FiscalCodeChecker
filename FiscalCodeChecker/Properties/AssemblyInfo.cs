b815e9aa44e85694d52541441a2b20addcae80c2
supix <esposito.marce@gmail.com>
2016-10-22 22:53:41 +0200

    AssemblyInfo contains information useful for deploy

--- FiscalCodeChecker/Properties/AssemblyInfo.cs
+++ FiscalCodeChecker/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FiscalCodeChecker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This package contains an helper class to formally validate Italian fiscal codes")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("supix")]
 [assembly: AssemblyProduct("FiscalCodeChecker")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("supix")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
